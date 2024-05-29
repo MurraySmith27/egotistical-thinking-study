268af6943ff3b24336a382db17482887e631cb43
Murray Smith <55713649+MurraySmith27@users.noreply.github.com>
2024-05-29 17:21:30 -0400

    jkl;

--- Egotistical_Thinking_Study/Assets/UI/ExperimenterView/OrderCreationScreen/ExperimenterViewController.cs
+++ Egotistical_Thinking_Study/Assets/UI/ExperimenterView/OrderCreationScreen/ExperimenterViewController.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.Netcode;
 using Unity.VisualScripting;
-using UnityEditor;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.UIElements;
 
